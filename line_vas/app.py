4a091fa700664eeafa849f09b56742df5881f9a9
yudai <plentyof258@gmail.com>
2024-03-08 21:51:18 +0900

    20240307-test

---
+++ line_vas/app.py
@@ -0,0 +1,16 @@
+import os
+import config
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from google.api_core.client_options import ClientOptions
+from google.cloud import discoveryengine
+
+app = Flask(__name__)
+
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))
+    app.run(debug=False, host='0.0.0.0', port=port)
---
+++ line_vas/config.py
@@ -0,0 +1,7 @@
+token = "アクセストークン"
+secret = "チャネルシークレット"
+project_id = "プロジェクトID"
+location = "global"
+datastore = "データストアID"
+# OPENAI_API_KEY = "YOUR_OPENAI_API_KEY"
+# TAVILY_API_KEY = 
